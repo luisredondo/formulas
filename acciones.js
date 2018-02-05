07f121f018c35e762dfdd2a8eda386c5dce6768a
luisfernandor <31326194+luisfernandor@users.noreply.github.com>
2018-02-04 22:04:12 -0600

    Add files via upload

---
+++ acciones.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#boton").on("click", function(){
+        $("#info").toggle(400); 
+    }); 
+    $("#boton1").on("click", function(){
+        $("#info1").toggle(400); 
+    }); 
+    $("#boton2").on("click", function(){
+        $("#info2").toggle(400); 
+    }); 
+    $("#boton3").on("click", function(){
+        $("#info3").toggle(400); 
+    }); 
+    $("#boton4").on("click", function(){
+        $("#info4").toggle(400); 
+    }); 
+    $("#boton5").on("click", function(){
+        $("#info5").toggle(400); 
+    }); 
+});
